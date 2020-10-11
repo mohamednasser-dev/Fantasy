645f878845a2390d7b4b977f237584b7ae90de26
mohamednasser2019 <mimonasser50@gmail.com>
2020-10-11 21:29:02 +0200

    nasser 2

--- football.sql
+++ football.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2020 at 07:24 PM
+-- Generation Time: Oct 11, 2020 at 09:28 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
@@ -57,7 +57,10 @@ CREATE TABLE `clubs` (
 
 INSERT INTO `clubs` (`id`, `club_name`, `tournaments`, `date_created`, `desc`, `image`, `created_at`, `updated_at`) VALUES
 (1, 'barshelona', 'sgh', '2020-10-11', 'sd', 'img_1602382719.jpg', '2020-10-11 02:00:54', '2020-10-11 02:19:22'),
-(2, 'real madrid', 'dfgfx', '2020-10-11', 'adsf', 'img_1602382581.png', '2020-10-11 02:16:21', '2020-10-11 02:16:21');
+(2, 'real madrid', 'dfgfx', '2020-10-11', 'adsf', 'img_1602382581.png', '2020-10-11 02:16:21', '2020-10-11 02:16:21'),
+(3, 'liverpool', 'dfg', '2020-10-11', 'df', 'img_1602442038.png', '2020-10-11 18:46:20', '2020-10-11 18:47:18'),
+(5, 'default club', 'fsgd', '2020-09-30', 'sadfs', 'default_club.png', '2020-10-11 19:04:39', '2020-10-11 19:04:39'),
+(6, 'asdfds', 'sdf', '2020-10-09', 'sfg', 'default_club.png', '2020-10-11 19:09:48', '2020-10-11 19:09:48');
 
 -- --------------------------------------------------------
 
@@ -81,7 +84,10 @@ CREATE TABLE `coaches` (
 --
 
 INSERT INTO `coaches` (`id`, `coach_name`, `age`, `image`, `desc`, `club_id`, `created_at`, `updated_at`) VALUES
-(1, 'parcha coach', 47, 'img_1602382566.jpg', 'ertsyu', 1, '2020-10-11 02:16:06', '2020-10-11 03:09:21');
+(1, 'parcha coach', 47, 'img_1602382566.jpg', 'ertsyu', 1, '2020-10-11 02:16:06', '2020-10-11 03:09:21'),
+(2, 'zineden zidan', 48, 'img_1602441161.jpg', 'sfdg', 2, '2020-10-11 18:32:41', '2020-10-11 18:32:41'),
+(3, 'jurgen klopp', 40, 'img_1602442097.jpg', 'بيلا', 3, '2020-10-11 18:47:59', '2020-10-11 18:48:17'),
+(5, 'default coach', 30, 'default_coach.png', '\\sadsf', 5, '2020-10-11 19:04:53', '2020-10-11 19:04:53');
 
 -- --------------------------------------------------------
 
@@ -125,7 +131,9 @@ CREATE TABLE `matches` (
 
 INSERT INTO `matches` (`id`, `home_club_id`, `away_club_id`, `time`, `date`, `home_score`, `away_score`, `status`, `stadium_id`, `tour_id`, `created_at`, `updated_at`) VALUES
 (3, 1, 2, '10:30:00', '2020-10-11', 1, 1, 'started', 1, 3, '2020-10-11 15:56:11', '2020-10-11 16:57:43'),
-(4, 2, 1, '16:30:00', '2020-10-11', 0, 0, 'not started', 1, 3, '2020-10-11 17:18:46', '2020-10-11 17:18:46');
+(4, 2, 1, '16:30:00', '2020-10-11', 0, 0, 'not started', 1, 3, '2020-10-11 17:18:46', '2020-10-11 17:18:46'),
+(5, 1, 2, '10:30:00', '2020-10-28', 2, 7, 'not started', 1, 3, '2020-10-11 18:49:49', '2020-10-11 19:03:41'),
+(6, 1, 3, '10:30:00', '2020-10-28', 0, 0, 'not started', 1, 3, '2020-10-11 19:11:38', '2020-10-11 19:11:38');
 
 -- --------------------------------------------------------
 
@@ -191,7 +199,8 @@ CREATE TABLE `players` (
 
 INSERT INTO `players` (`id`, `player_name`, `age`, `center_name`, `desc`, `image`, `club_id`, `created_at`, `updated_at`) VALUES
 (1, 'Lionel Messi', '34', 'RM', 'fdghf', 'img_1602382785.jpg', 1, '2020-10-11 02:19:45', '2020-10-11 17:16:52'),
-(2, 'neymar', '203', 'LWB', 'dsdfhg', 'img_1602382872.jpg', 1, '2020-10-11 02:21:12', '2020-10-11 02:21:12');
+(2, 'neymar', '203', 'LWB', 'dsdfhg', 'img_1602382872.jpg', 1, '2020-10-11 02:21:12', '2020-10-11 02:21:12'),
+(4, 'mohamed salah', '28', 'CDM', 'safdsg', 'default_player.png', 1, '2020-10-11 18:26:33', '2020-10-11 18:26:33');
 
 -- --------------------------------------------------------
 
@@ -213,7 +222,8 @@ CREATE TABLE `stadiums` (
 
 INSERT INTO `stadiums` (`id`, `stadium_name`, `image`, `created_at`, `updated_at`) VALUES
 (1, 'stadium 1', 'img_1602431753.jpg', '2020-10-11 15:55:53', '2020-10-11 15:55:53'),
-(2, 'stadium num2', 'img_1602436636.JPG', '2020-10-11 17:17:16', '2020-10-11 17:17:16');
+(2, 'stadium num2', 'img_1602436636.JPG', '2020-10-11 17:17:16', '2020-10-11 17:17:16'),
+(3, 'default', 'default_stadium.png', '2020-10-11 19:01:17', '2020-10-11 19:01:17');
 
 -- --------------------------------------------------------
 
@@ -350,13 +360,13 @@ ALTER TABLE `centers`
 -- AUTO_INCREMENT for table `clubs`
 --
 ALTER TABLE `clubs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `coaches`
 --
 ALTER TABLE `coaches`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -368,7 +378,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `matches`
 --
 ALTER TABLE `matches`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -380,13 +390,13 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `players`
 --
 ALTER TABLE `players`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `stadiums`
 --
 ALTER TABLE `stadiums`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tournaments`
