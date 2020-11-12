4d8c026e09002c487086a5022cb758bf78a4287e
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2020-11-12 23:22:26 +0200

    forget password validation

--- app/Http/Controllers/ForgotPasswordController.php
+++ app/Http/Controllers/ForgotPasswordController.php
@@ -2,8 +2,10 @@
 namespace App\Http\Controllers;
 use App\ApiCode;
 use App\Http\Requests\ResetPasswordRequest;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Http\Request;
+use App\User;
 class ForgotPasswordController extends Controller
 {
     public function sendResponse($code = null, $msg = null, $data = null){
@@ -32,20 +34,39 @@ class ForgotPasswordController extends Controller
         }
     }
     public function forgot() {
-        $credentials = request()->validate(['email' => 'required|email']);
-        Password::sendResetLink($credentials);
-        return $this->sendResponse(200,'Reset password link sent on your email.',null);
+        $input = request()->all();
+        $email = request()->input('email');
+        $user = User::where('email',$email)->first();
+        if ($user != null) 
+        {
+            $credentials = request()->validate(['email' => 'required|email|exists:users,email']);
+            if (count((array)$credentials) == 1) 
+            {
+                Password::sendResetLink($credentials);
+                $data['title_message']=  'Reset password link sent on your email.';
+                return $this->sendResponse(200,'Reset password link sent on your email.',$data);
+            }
+        }else {
+            return $this->sendResponse(403, 'your email is invaled ...!', null);
+        }    
     }
     public function reset(ResetPasswordRequest $request) {
-        
-        $reset_password_status = Password::reset($request->validated(), function ($user, $password) {
-            $user->password =bcrypt($password);
-            $user->save();
-        });
+        $input = $request->all();
+        $email = $request->input('email');
+        $user = User::where('email',$email)->first();
+        if ($user != null) 
+        {
+            $reset_password_status = Password::reset($request->validated(), function ($user, $password) {
+                $user->password =bcrypt($password);
+                $user->save();
+            });
 
-        if ($reset_password_status == Password::INVALID_TOKEN) {
-        	return $this->sendResponse(403,"Token invaled" ,null);
-        }
-        return $this->sendResponse(200,"Password has been successfully changed",null);
+            if ($reset_password_status == Password::INVALID_TOKEN) {
+            	return $this->sendResponse(403,"Token invaled" ,null);
+            }
+            return $this->sendResponse(200,"Password has been successfully changed",null);
+        }else {
+            return $this->sendResponse(403, 'your email is invaled ...!', null);
+        } 
     }
 }
